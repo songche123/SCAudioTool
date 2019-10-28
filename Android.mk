b48708a6405645716a90dc3300e544937ab20193
songche123 <809626366@qq.com>
2019-10-28 16:29:05 +0800

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,1 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
 
+include $(CLEAR_VARS)
+
+#LOCAL_CFLAGS += -DAML_CONFIG_SUPPORT_READ_ONLY
+
+LOCAL_C_INCLUDES +=                      \
+    $(LOCAL_PATH)/include       
+
+
+LOCAL_SRC_FILES  +=               \
+    volume.c
+
+LOCAL_MODULE := libscaudiotool
+
+ifeq ($(shell test $(PLATFORM_SDK_VERSION) -ge 26 && echo OK),OK)
+    LOCAL_PROPRIETARY_MODULE := true
+endif
+
+LOCAL_SHARED_LIBRARIES += \
+    libc                  \
+    libcutils             \
+    libutils              \
+    liblog                
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS += -DBUILD_IN_ANDROID
+
+include $(BUILD_SHARED_LIBRARY)
